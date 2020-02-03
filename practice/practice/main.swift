b5d05c5fe54116917bfb73053051938c1045aea1
gombome <persistence10@naver.com>
2020-02-03 17:32:42 +0900

    no message

--- practice/practice/main.swift
+++ practice/practice/main.swift
@@ -13,24 +13,29 @@
 
 import Foundation
 
-let conversionInt: Int = 0
-
-
-
 //Model
 struct Model {
-    
-    
 }
 
 
 //Controller
 
-enum ExceptionLog: String, Swift.Error {
-    case invaildValue = "입력한 값이 올바르지 않습니다."
-    case failunwrapping = "언래핑 실패"
-    case failinputAges = "나이값 오기입"
+enum ExceptionLog: Int, Swift.Error, CustomStringConvertible {
+    case invaildValue = -999   //= "입력한 값이 올바르지 않습니다."
+    case failunwrapping = -998 //= "언래핑 실패"
+    case failinputAges = -997  //= "나이값 오기입"
     
+    var description: String {
+        
+        switch self {
+        case .invaildValue:
+            return "입력한 값이 올바르지 않습니다."
+        case .failinputAges:
+            return "나이값 오기입"
+        case .failunwrapping:
+            return "언래핑 실패"
+        }
+    }
 }
 
 func conversionStringToInt (_ inputValue: String) throws -> Int {
@@ -40,33 +45,35 @@ func conversionStringToInt (_ inputValue: String) throws -> Int {
     }
     
     return conversionInt
-   
-}
     
+}
+
 // Match your age
 
-    func matchYourAge (_ converionInt: Int) throws -> Int {
-        guard conversionInt >= 0 else {
-            throw ExceptionLog.failinputAges
-        }
-        
-        switch conversionInt {
-        case 1..<6:
-            print("Ur infant")
-        case 6..<14:
-            print("Ur Child")
-        case 14..<20:
-            print("Ur teenager")
-        case 20..<60:
-            print("Ur adult")
-        default:
-            print("Ur the aged")
-        }
-        
-        return 0
+func matchYourAge (from conversionInt: Int) throws -> Int {
+    guard conversionInt >= 0 else {
+        throw ExceptionLog.failinputAges
     }
-
     
+    switch conversionInt {
+    case 0..<6:
+        print("Ur infant")
+    case 6..<14:
+        print("Ur Child")
+    case 14..<20:
+        print("Ur teenager")
+    case 20..<60:
+        print("Ur adult")
+    default:
+        print("Ur the aged")
+    }
+    
+    
+    
+    return 0
+}
+
+
 
 //View
 
@@ -80,30 +87,29 @@ func inputAges() throws -> String {
 
 
 func main() {
-        
+    
     do  {
         let inputValue = try inputAges() // String?->String
         let conversion = try conversionStringToInt(_ : inputValue) // String->Int
-        let calculation = try matchYourAge(_ : conversion) // calculation Ages
+        let calculation = try matchYourAge(from: conversion) // calculation Ages
         
     }catch let error {
-//        let errorCode = (error as NSError).code
-//        guard let exceptionLog = ExceptionLog(rawValue: errorCode) else{
-//            return
-//        }
-//
-//        switch exceptionLog {
-//        case .invaildValue:
-//        print(ExceptionLog.invaildValue)
-//        case .failunwrapping:
-//        print(ExceptionLog.failunwrapping)
-//        case .failinputAges:
-//        print (ExceptionLog.failinputAges)
-//        }
+        let errorCode = (error as NSError).code
+        guard let exceptionLog = ExceptionLog(rawValue: errorCode) else{
+            return
+        }
+        switch exceptionLog {
+        case .invaildValue:
+            print(ExceptionLog.invaildValue)
+        case .failunwrapping:
+            print(ExceptionLog.failunwrapping)
+        case .failinputAges:
+            print (ExceptionLog.failinputAges)
+        }
         
-     }
- }
-    
+    }
+}
+
 
 
 main()
@@ -117,29 +123,32 @@ main()
  1.
  enum ExceptionLog: String 만 했을 떄 에러 발생. (어래와 같이 ㅠ)
  Thrown expression type 'ErrorLog' does not conform to 'Error'
-
+ -> Swift.Error 으로 throw(에러)를 받아야 에러 발생하지 않음.
+ -> throw 도 retrun 타입이 결국은 error 타입
+ 
+ -> let errorCode = (error as NSError).code 은 error변수의 as NSError.code ( object-c 안의 NSError를 포함하는 코드 )
  
  2.
  func main() {} 에서 catch 예외처리 미숙
+ -> 다시 복습 해 볼 것.
  
  
  3.
  Mvc model 에서 model 부분 없음
+ 
  3-1
  func matchYourAge 부분 swift 문 결과 값 View 로 이동하고 싶음
+ -> Mvc modeling 다시 생각해볼 것.
  
  
  4.
-   conversionStringToInt(_ : inputValue)
-   converionStringToInt(_ inputValue: inputvalue) <-- 이거는 왜 안되는지, 차이가 뭔지...??
+ conversionStringToInt(_ : inputValue)
+ -> conversionStringToInt(from : inputValue) 같은말임
  
- 5.
-    catch let error {
-    let errorCode = (error as NSError).code
-    guard let exceptionLog = ExceptionLog(rawValue: errorCode) else{
-        return
-    }    위 구문 이해 못하겠음.
- 
+ converionStringToInt(_ inputValue: inputvalue) <-- 이거는 왜 안되는지, 차이가 뭔지...??
+ -> 문법차이.!
  
  
  */
+
+
